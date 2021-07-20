c7b3ac48b06f4eb25388b29c5febc10fe33f143f
搋兔 <naruto6404@hotmail.com>
2021-07-20 14:44:11 +0800

    fix: code format

--- 2021-07-20 (day2)/huibizhang/solution.js
+++ 2021-07-20 (day2)/huibizhang/solution.js
@@ -10,7 +10,7 @@
     let num = x
     let num_array = []
     while(num>0){
-    	num_array.push( num%10 )
+    num_array.push( num%10 )
       num = Math.floor(num/10)
     }
     
@@ -22,6 +22,6 @@
     if(x< -1*Math.pow(2,31) || x > ((Math.pow(2,31) - 1))){
         return 0
     }else{
-    		return x===num
+    	return x===num
     }
 };
