04ce6fd84913a1d16a993f626a70d7c4453716ca
beareatapple <beareatapple@gmail.com>
2021-07-23 22:12:03 +0800

    day5 homework done!

--- 2021-07-22 (day4)/white/day4_valid_parentheses.py
+++ 2021-07-22 (day4)/white/day4_valid_parentheses.py
@@ -1,21 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 題目要求的長度範圍
-        if (1<= len(s) <= 10**4) :
-            # 括號們用dict兩兩配對
-            pairs = {'(':')', '[':']','{':'}'}
-            stack = []
+        pairs = {'(':')', '[':']','{':'}'}
+        stack = []
+        
+        for char in s:
+            if char in pairs.keys():
+                stack.append(char)
+            elif stack:
+                if pairs.get(stack.pop()) != char:
+                    return False
+            else:
+                return False
             
-            for char in s:
-                # 如果讀到的字元是屬於 dict的 key值(即左括號) 放進stack備用
-                if char in pairs.keys():
-                    stack.append(char)
-                else:
-                    # 如果是右括號 把stack裡的東西拿出來比較
-                    if stack:
-                        if pairs.get(stack.pop()) != char:
-                            return False
-                    else:
-                        return False
-            # 正確情況下字元全數讀完後 stack必須為空
-            return True if not stack else False
+        return True if not stack else False
