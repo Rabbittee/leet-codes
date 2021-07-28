3e7e822904a2a580a6414205bf78074aaf548a3b
Alpha <alphatero@icloud.com>
2021-07-28 20:51:00 +0800

    add alpha's solution with js

---
+++ 2021-07-28 (day10)/Alpha/README.md
@@ -0,0 +1,3 @@
+## Solution
+
+**created an object about specific Roman numeral,and use for loop to check every object key,and decriced value to 0 then added to string**
---
+++ 2021-07-28 (day10)/Alpha/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ var intToRoman = function(num) {
+  let str = "";
+  const romals = {
+     1000: "M",
+      900: "CM",
+      500: "D",
+      400: "CD",
+      100: "C",
+      90: "XC",
+      50: "L",
+      40: "XL",
+      10: "X",
+      9: "IX",
+      5: "V",
+      4: "IV",
+      1: "I"
+  }; 
+
+
+for(let i=Object.keys(romals).length; i>=0; i--) {
+while(num>=Object.keys(romals)[i]){
+  num-=Object.keys(romals)[i];
+  str+=romals[Object.keys(romals)[i]];
+}
+}
+
+  return str;
+};
