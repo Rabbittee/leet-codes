13a303c75f8f6fd9c23d6c26069123ada6026569
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-08-05 20:42:37 +0800

    fixed solution4.py

--- 2021-08-05 (day18)/CXPhoenix/solution4.py
+++ 2021-08-05 (day18)/CXPhoenix/solution4.py
@@ -4,6 +4,6 @@ class Solution:
         for i in range(len(nums)):
             toFind = target - nums[i]
             try:
-                return [i, record[toFind]] if i <= record[toFind] else [record[toFind], i]
+                return [record[toFind], i]
             except:
                 record[nums[i]] = i
