405daecc3b7f4d68161dba734aceb815b7663ff4
HuskyHsu <schi.hsu@gmail.com>
2021-08-07 22:28:33 +0800

    update use set

--- 2021-08-07 (day20)/Husky/solution_2.py
+++ 2021-08-07 (day20)/Husky/solution_2.py
@@ -1,11 +1,11 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        uniq = []
+        uniq = set()
         for i, char in enumerate(s):
             if char not in uniq:
                 if char not in s[i+1:]:
                     return i
                 else:
-                    uniq.append(char)
+                    uniq.add(char)
 
         return -1
