ad7a4440f103e1fc5ff42d0ad8663d8329fcacba
kayac-chang <egg734631@gmail.com>
2021-08-15 23:20:27 +0800

    add recursion version

--- 2021-08-15 (day28)/Hello/two-point.js
+++ 2021-08-15 (day28)/Hello/two-point.js
@@ -2,7 +2,6 @@
  * @param {number[]} nums
  * @return {number}
  */
-<<<<<<< HEAD
 function removeDuplicates(nums, point = 0) {
   const cursor = nums.lastIndexOf(nums[point]) + 1;
 
@@ -13,25 +12,4 @@ function removeDuplicates(nums, point = 0) {
   }
 
   return removeDuplicates(nums, point);
-=======
-function removeDuplicates(nums) {
-  let point = 0;
-
-  while (true) {
-    let cursor = nums.lastIndexOf(nums[point]) + 1;
-
-    if (cursor >= nums.length) break;
-
-    if (cursor === point) continue;
-
-    point += 1;
-    nums[point] = nums[cursor];
-  }
-
-<<<<<<< HEAD
-  return point;
->>>>>>> bb9e6be (add two pointer)
-=======
-  return point + 1;
->>>>>>> 8bf3d19 (fix)
 }
