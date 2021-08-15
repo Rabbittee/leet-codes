640f427c50f2e65334091873efa3a7b9fe94d211
kayac-chang <egg734631@gmail.com>
2021-08-15 22:47:01 +0800

    add two pointer

---
+++ 2021-08-15 (day28)/Hello/two-point.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+  let point = 0;
+
+  while (true) {
+    let cursor = nums.lastIndexOf(nums[point]) + 1;
+
+    if (cursor >= nums.length) break;
+
+    if (cursor === point) continue;
+
+    point += 1;
+    nums[point] = nums[cursor];
+  }
+
+  return point;
+}
