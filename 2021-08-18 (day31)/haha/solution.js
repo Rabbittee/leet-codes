a604f813bf5eeb22534cb3044b4bbd6273214be2
haha861924 <qq861924@gmail.com>
2021-08-18 16:13:19 +0800

    fix solution use ternary operator

--- 2021-08-18 (day31)/haha/solution.js
+++ 2021-08-18 (day31)/haha/solution.js
@@ -5,10 +5,8 @@
 var singleNumber = function (nums) {
     let map = new Map()
     nums.forEach((el, index) => {
-        // if each one has in map, delete it
-        if (map.has(el)) map.delete(el)
-        // or not add it
-        else map.set(el, index)
+        // if each one has in map, delete it, or not add it
+        map.has(el) ? map.delete(el) : map.set(el, index)
     })
     return [...map.keys()]
 };
