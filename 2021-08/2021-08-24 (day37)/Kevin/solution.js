5319ae20fd5f161767b208e87457debd99f5abf9
kevin_hws <kevinshu1995@gmail.com>
2021-08-24 22:06:40 +0800

    add kevin day 37

---
+++ 2021-08/2021-08-24 (day37)/Kevin/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let result = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            result += prices[i] - prices[i - 1];
+        }
+    }
+    return result;
+};
