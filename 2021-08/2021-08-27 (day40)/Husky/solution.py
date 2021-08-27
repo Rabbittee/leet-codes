4a98962c8d1d3fbcaa95dbf8bc77a8720b573810
HuskyHsu <schi.hsu@gmail.com>
2021-08-27 16:28:31 +0800

    update solution

--- 2021-08/2021-08-27 (day40)/Husky/solution.py
+++ 2021-08/2021-08-27 (day40)/Husky/solution.py
@@ -2,7 +2,10 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp = [0]*(len(cost)+1)
 
-        for i in range(2, len(cost)+1):
-            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+        if len(cost) > 2:
+            for i in range(2, len(cost)+1):
+                dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+        else:
+            dp[2] = min(cost[0], cost[1])
 
         return dp[-1]
