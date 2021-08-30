c008b08167ded92de91b9e332903c5802d189f19
kayac-chang <egg734631@gmail.com>
2021-08-30 14:47:25 +0800

    refactor

--- 2021-08/2021-08-30 (day43)/Hello/solution.js
+++ 2021-08/2021-08-30 (day43)/Hello/solution.js
@@ -1,19 +1,22 @@
+function range(n) {
+  return Array.from({ length: n }, (_, index) => index);
+}
+
+function append(arr, item) {
+  return [...arr, item];
+}
+
 /**
  * @param {number} numRows
  * @return {number[][]}
  */
-function generate(
-  numRows,
-  result = [],
-  lastrow = result[result.length - 1] || []
-) {
-  if (result.length === numRows) return result;
-
-  return generate(numRows, [
-    ...result,
-    Array.from(
-      { length: lastrow.length + 1 },
-      (_, x) => lastrow[x - 1] + lastrow[x] || 1
-    ),
-  ]);
+function generate(numRows) {
+  return range(numRows).reduce((result, y) => {
+    return append(
+      result,
+      !result[y - 1]
+        ? [1]
+        : range(y + 1).map((x) => result[y - 1][x - 1] + result[y - 1][x] || 1)
+    );
+  }, []);
 }
