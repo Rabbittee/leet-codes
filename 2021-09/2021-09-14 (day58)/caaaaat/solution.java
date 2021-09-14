8b3beebd0e2fccbf5dd584af6b38eb3847dee884
william_chan <william830724@gmail.com>
2021-09-14 21:45:01 +0800

    add solution.java
    day 58

---
+++ 2021-09/2021-09-14 (day58)/caaaaat/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        if(fast != null){
+             slow = slow.next;
+        }
+        slow = reverse(slow);
+        while (slow != null) {
+            if (head.val != slow.val) 
+                return false;
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
