1bf51945c1525af3bf97094123cb0c5ce716ca37
Sid-Straw <j80304xz@gmail.com>
2021-09-19 14:17:24 +0800

    feat: init 622. Design Circular Queue

---
+++ 2021-09/2021-09-19 (day63)/Sid/README.md
@@ -0,0 +1,20 @@
+# 622. Design Circular Queue
+
+<https://leetcode.com/problems/design-circular-queue/>
+
+## Code
+
+```JS
+
+```
+
+## 解題想法
+
+
+
+## leecode 提交結果
+
+
+
+## 備註心得
+
---
+++ 2021-09/2021-09-19 (day63)/Sid/solution.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number} k
+ */
+var MyCircularQueue = function (k) {}
+
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function(value) {
+    
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+    
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+    
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+    
+};
+
+/** 
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
