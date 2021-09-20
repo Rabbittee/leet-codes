b91e9897c7641f2066b3f79f07303247dc5b8108
Sid-Straw <j80304xz@gmail.com>
2021-09-20 23:54:06 +0800

    feat: No.622 Status: Accepted

--- 2021-09/2021-09-19 (day63)/Sid/solution.js
+++ 2021-09/2021-09-19 (day63)/Sid/solution.js
@@ -1,52 +1,71 @@
 /**
  * @param {number} k
  */
-var MyCircularQueue = function (k) {}
+function MyCircularQueue(k) {
+  const queue = Array(k)
+  let head = -1
+  let tail = -1
+  /**
+   * @param {number} value
+   * @return {boolean}
+   */
+  const enQueue = value => {
+    if (isFull()) return false
+    if (isEmpty()) head = 0
+    queue[(tail = (tail + 1) % k)] = value
+    return true
+  }
+  /**
+   * @return {boolean}
+   */
+  const deQueue = () => {
+    if (isEmpty()) return false
+    if (head === tail) {
+      head = -1
+      tail = -1
+      return true
+    }
+    head = (head + 1) % k
+    return true
+  }
+  /**
+   * @return {number}
+   */
+  const Front = () => {
+    return isEmpty() ? -1 : queue[head]
+  }
+  /**
+   * @return {number}
+   */
+  const Rear = () => {
+    return isEmpty() ? -1 : queue[tail]
+  }
+  /**
+   * @return {boolean}
+   */
+  const isEmpty = () => {
+    if (head === -1) return true
+    return false
+  }
+  /**
+   * @return {boolean}
+   */
+  const isFull = () => {
+    if ((head + k - 1) % k === tail) return true
+    return false
+  }
 
-/** 
- * @param {number} value
- * @return {boolean}
- */
-MyCircularQueue.prototype.enQueue = function(value) {
-    
-};
+  return {
+    enQueue,
+    deQueue,
+    Front,
+    Rear,
+    isEmpty,
+    isFull,
+  }
+}
 
 /**
- * @return {boolean}
- */
-MyCircularQueue.prototype.deQueue = function() {
-    
-};
-
-/**
- * @return {number}
- */
-MyCircularQueue.prototype.Front = function() {
-    
-};
-
-/**
- * @return {number}
- */
-MyCircularQueue.prototype.Rear = function() {
-    
-};
-
-/**
- * @return {boolean}
- */
-MyCircularQueue.prototype.isEmpty = function() {
-    
-};
-
-/**
- * @return {boolean}
- */
-MyCircularQueue.prototype.isFull = function() {
-    
-};
-
-/** 
  * Your MyCircularQueue object will be instantiated and called as such:
  * var obj = new MyCircularQueue(k)
  * var param_1 = obj.enQueue(value)
@@ -56,3 +75,14 @@ MyCircularQueue.prototype.isFull = function() {
  * var param_5 = obj.isEmpty()
  * var param_6 = obj.isFull()
  */
+
+var circularQueue = new MyCircularQueue(3) // 設置長度為 3
+console.log('true : ', circularQueue.enQueue(1)) // 返回 true
+console.log('true : ', circularQueue.enQueue(2)) // 返回 true
+console.log('true : ', circularQueue.enQueue(3)) // 返回 true
+console.log('false : ', circularQueue.enQueue(4)) // 隊列已滿返回 false
+console.log('3 : ', circularQueue.Rear()) // 返回 3
+console.log('true : ', circularQueue.isFull()) // 返回 true
+console.log('true : ', circularQueue.deQueue()) // 返回 true
+console.log('true : ', circularQueue.enQueue(4)) // 返回 true
+console.log('4 : ', circularQueue.Rear()) // 返回 4
