626fd57581a100e876378717ede49758fabca3d9
雷承憲 <leichengxian@tpea90076135.whq.wistron>
2021-09-23 12:29:23 +0800

    record the Day66 Ray solution

---
+++ 2021-09/2021-09-23 (day66)/Ray/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    // use queue to store the s string
+    let queue = s.split("");
+    let compareElement = queue.shift("");
+    
+    for (let i = 0 ; i < t.length ; i++) {
+        if (compareElement === t[i]) {
+            compareElement = queue.shift("");
+        }
+    }
+    // if queue is empty and final compareElement is be compared
+    // the compareElement will be undefined
+    return !compareElement;
+};
