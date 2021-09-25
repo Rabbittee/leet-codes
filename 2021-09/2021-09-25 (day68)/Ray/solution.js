95e9c4ec09ef43e9ba21c32c5b49323f2f26d13a
雷承憲 <leichengxian@leichengxians-MacBook-Pro.local>
2021-09-25 13:26:41 +0800

    just add space for beauty

--- 2021-09/2021-09-25 (day68)/Ray/solution.js
+++ 2021-09/2021-09-25 (day68)/Ray/solution.js
@@ -6,16 +6,16 @@
 const calcFactorial = (num) => {
     let factorial = 1;
     for (let i = 1 ; i <= num ; i++) {
-        factorial = factorial*i;
+        factorial = factorial * i;
     }
     return factorial;
 }
 
 var uniquePaths = function(m, n) {
-    m=m-1;
-    n=n-1;
+    m = m - 1;
+    n = n - 1;
     
-    return calcFactorial(m + n) / (calcFactorial(m)*calcFactorial(n))
+    return calcFactorial(m + n) / (calcFactorial(m) * calcFactorial(n))
 };
 
 // Runtime: 68 ms, faster than 91.67% of JavaScript online submissions for Unique Paths.
