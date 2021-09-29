a6ae5f2007d2166262fbe606fd8d0d4c3f501392
Augustine <ashramwen@gmail.com>
2021-09-29 23:15:52 +0800

    fix: 🐛 Fix empty array issue

--- 2021-09/2021-09-29 (day72)/aug/solution.js
+++ 2021-09/2021-09-29 (day72)/aug/solution.js
@@ -9,7 +9,7 @@ var permute = function (nums) {
 function getPermutations(nums, ans = [], res = []) {
   if (ans.length === nums.length) {
     res.push(ans);
-    return;
+    return [];
   }
 
   const rest = nums.filter((n) => !ans.includes(n));
@@ -20,3 +20,12 @@ function getPermutations(nums, ans = [], res = []) {
 
   return res;
 }
+
+/**
+ * https://leetcode.com/problems/permutations/
+ * 46. Permutations
+ *
+ * Runtime: 100 ms, faster than 39.92% of JavaScript online submissions for Permutations.
+ * Memory Usage: 44.6 MB, less than 7.47% of JavaScript online submissions for Permutations.
+ *
+ */
