b44fdb2eb27af47d8c0657d8385af2fb5fe5b815
White Bunny <beareatapple@gmail.com>
2021-10-08 09:11:27 +0800

    add White's day 81

---
+++ 2021-10/2021-10-08 (day81)/White/README.md
@@ -0,0 +1,6 @@
+# 解題想法
+相同數量的L跟R是一組結果，故當L和R數量相同時，答案數加一。
+
+# leetcode 提交結果
+Runtime: 24 ms, faster than 96.79% of Python3 online submissions for Split a String in Balanced Strings.
+Memory Usage: 14.1 MB, less than 69.00% of Python3 online submissions for Split a String in Balanced Strings.
---
+++ 2021-10/2021-10-08 (day81)/White/balancedStringSplit.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        ans = l = 0
+        for c in s:
+            if c == "L":
+                l += 1
+            else:
+                l -= 1
+            
+            if l == 0:
+                ans += 1
+                
+        return ans
