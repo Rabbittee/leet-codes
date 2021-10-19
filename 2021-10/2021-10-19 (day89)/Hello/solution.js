fbddc012204ceb66d1274e56133bdc5562c51579
kayac-chang <egg734631@gmail.com>
2021-10-19 17:28:17 +0800

    remove unuse

--- 2021-10/2021-10-19 (day89)/Hello/solution.js
+++ 2021-10/2021-10-19 (day89)/Hello/solution.js
@@ -1,9 +1,3 @@
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
-
 /**
  * @param {TreeNode} root
  * @return {boolean}
