bb9ed85c77722516ea0a29610159997b16658309
雷承憲 <leichengxian@tpea90092241.whq.wistron>
2021-10-19 11:17:18 +0800

    record the answer

---
+++ 2021-10/2021-10-19 (day89)/Ray/solution.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+    let result = true;
+    let uniValue = root.val;
+    const recursive = (nowNode) => {
+        if (nowNode.val !== uniValue) {
+            result = false;
+            return;
+        }
+        if (!nowNode.left && !nowNode.right) {
+            return;
+        }
+        if (nowNode.left) {
+            recursive(nowNode.left);
+        }
+        if (nowNode.right) {
+            recursive(nowNode.right);
+        }
+    };
+    recursive(root);
+        
+    return result;
+};
