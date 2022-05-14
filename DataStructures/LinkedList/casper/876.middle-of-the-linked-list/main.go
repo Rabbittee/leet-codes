ea4dceb001a74e89c55a4680386a2848b865db94
vn7n24fzkq <vn7n24fzkq@gmail.com>
2022-05-15 05:17:49 +0800

    style: go fmt

--- DataStructures/LinkedList/casper/876.middle-of-the-linked-list/main.go
+++ DataStructures/LinkedList/casper/876.middle-of-the-linked-list/main.go
@@ -1,12 +1,12 @@
 func middleNode(head *ListNode) *ListNode {
-	nodeArray := make([]*ListNode,100)
-	count := 0
+    nodeArray := make([]*ListNode,100)
+    count := 0
 
-	for head != nil {
-		nodeArray[count] = head
-		head = head.Next
-		count++
-	}
+    for head != nil {
+        nodeArray[count] = head
+        head = head.Next
+        count++
+    }
 
-	return nodeArray[count/2]
+    return nodeArray[count/2]
 }
